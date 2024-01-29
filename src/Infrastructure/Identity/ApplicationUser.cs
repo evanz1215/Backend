8d37d05948f74629e770b3b9ef83321490ecd398
Evan <evanz1215@gmail.com>
2024-01-29 22:16:19 +0800

    新增User和Role Identiy Entity

---
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity;
+
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string ImageUrl { get; set; }
+
+    public bool IsActive { get; set; }
+    public string RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiryTime { get; set; }
+    public string ObjectId { get; set; }
+}
